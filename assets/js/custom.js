d497eee05c097bd357c81673cfcceeafb129c21a
Surojit Saha <67891228+surojit729@users.noreply.github.com>
2024-10-01 01:02:39 +0530

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -347,7 +347,32 @@ jQuery(document).ready(function () {
       columnWidth: ".all",
     },
   })
+  // Function to check if all items are visible and add/remove class
+  function checkAllVisible() {
+    var totalItems = $grid.data("isotope").items.length
+    var visibleItems = $grid.isotope("getFilteredItemElements").length
 
+    // If all items are visible, add the 'all-visible' class
+    if (visibleItems === totalItems) {
+      $(".grid").addClass("all-visible")
+    } else {
+      $(".grid").removeClass("all-visible")
+    }
+  }
+
+  // Run check when Isotope is initialized and items are laid out
+  $grid.on("arrangeComplete", function () {
+    checkAllVisible() // Check visibility on each filtering action
+  })
+
+  // Run the check on initial page load
+  checkAllVisible()
+
+  // Filter items on button click
+  $(".filter-buttons").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter")
+    $grid.isotope({ filter: filterValue })
+  })
   var wpcf7Elm = document.querySelector(".getInTouchWrapper .wpcf7")
 
   wpcf7Elm.addEventListener(
